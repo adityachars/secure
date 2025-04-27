92b8d1e078f5e22f70ce1af957aa1f7cbb76bca7
adityachars <44857643+adityachars@users.noreply.github.com>
2025-04-27 18:04:01 +0530

    Update IRvm-internal.tf

--- ADF-Infrastructure/devops/terraform/IRvm-internal.tf
+++ ADF-Infrastructure/devops/terraform/IRvm-internal.tf
@@ -1,44 +1,46 @@
-
-resource "azurerm_lb" "internal-vmss-load-balancer" {
-  name                = var.internalvmsslbname[local.environment]
-  location            = var.location
+resource "azurerm_windows_virtual_machine" "internal-irvm-win" {
+  name                = var.internalIRvmName[local.environment]
   resource_group_name = data.azurerm_resource_group.rg.name
-  sku = "Standard"
+  location            = var.location
+  size                = var.irvmsize[local.environment]
+  computer_name       = var.computername[local.environment]
+  admin_username      = "adminuser"
+  admin_password      = azurerm_key_vault_secret.adf-integration-runtime-vm-password.value
+  network_interface_ids = [
+    azurerm_network_interface.az-vm-internal-nic.id,
+  ]
 
-  frontend_ip_configuration {
-    name                 = var.privatevmssipconfig[local.environment]
-    subnet_id =  azurerm_subnet.localvmsssubnet.id 
-    private_ip_address = var.private_ip[local.environment]
-    private_ip_address_allocation = var.lbaltype
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = var.irvmimageoffer[local.environment]
+    sku       = var.irvmsku[local.environment]
+    version   = "latest"
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "local-sftp-vmss-bpool" {
-  resource_group_name = data.azurerm_resource_group.rg.name
-  loadbalancer_id     = azurerm_lb.internal-vmss-load-balancer.id  
-  name                = "local-vmssBackEndAddressPool"
-
+resource "azurerm_management_lock" "internal-irvm-win" {
+  name       = var.internalIRvmName[local.environment]
+  scope      = azurerm_windows_virtual_machine.internal-irvm-win.id
+  lock_level = "CanNotDelete"
+  notes      = "Locked because you are not authorized to delete"
 }
 
-resource "azurerm_lb_rule" "local-sftp-vmss-load-balancer-rule" {
-  resource_group_name            = data.azurerm_resource_group.rg.name
-  loadbalancer_id                = azurerm_lb.internal-vmss-load-balancer.id 
-  name                           = "local-vmsslbalrule"
-  protocol                       = "Tcp"
-  frontend_port                  = var.internalvmsslbport[local.environment]
-  backend_port                   = 22
-  frontend_ip_configuration_name = azurerm_lb.internal-vmss-load-balancer.frontend_ip_configuration[0].name
-  backend_address_pool_ids = [azurerm_lb_backend_address_pool.local-sftp-vmss-bpool.id]
-  probe_id = azurerm_lb_probe.local-sftp-vmss-health-probe.id
-}
+# resource "azurerm_virtual_machine_extension" "internal-irvm-ext" {
+#   name                 = "internal-adf-IR"
+#   virtual_machine_id   = azurerm_windows_virtual_machine.internal-irvm-win.id 
+#   publisher            = "Microsoft.Compute"
+#   type                 = "CustomScriptExtension"
+#   type_handler_version = "1.10"
 
 
-resource "azurerm_lb_probe" "local-sftp-vmss-health-probe" {
-  resource_group_name = data.azurerm_resource_group.rg.name
-  loadbalancer_id     = azurerm_lb.internal-vmss-load-balancer.id
-  name                = "local-vmssprobe"
-  port                = 22
-  interval_in_seconds = 60
-  number_of_probes    = 3
-  protocol            = "tcp"
-}
+#   settings = jsonencode({
+#     "fileUris"         = ["https://raw.githubusercontent.com/Azure/azure-quickstart-templates/00b79d2102c88b56502a63041936ef4dd62cf725/101-vms-with-selfhost-integration-runtime/gatewayInstall.ps1"],
+#     "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File gatewayInstall.ps1 ${azurerm_data_factory_integration_runtime_self_hosted.northstaradfinternal.auth_key_1} && timeout /t 120"
+#   })
+
+# }
